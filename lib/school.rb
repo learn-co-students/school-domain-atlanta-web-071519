361ff4c2348be137269a72636ad1799b8fef1f12
John Odom <john.osborne.odom@gmail.com>
2019-07-17 15:52:22 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+
+    def initialize(name, roster = {})
+        @name=name
+        @roster=roster
+    end
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        hash = {}
+        self.roster.each do |key, value|
+            hash[key] = value.sort
+        end
+        hash
+    end
+
+end
